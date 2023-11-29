a9a7dc259f68e1960370b7e4ec47f00ee64e39dd
Gabs <gabs@fundz.money>
2023-11-29 15:15:45 -0300

    Layout created

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <h1 class="list-name">My tasks</h1>
   <!-- LIST COMPONENT -->
   <section class="wrap-list">
     <ul class="list-data">
@@ -13,19 +14,32 @@ import { RouterLink, RouterView } from 'vue-router'
     </ul>
   </section>
   <section class="wrap-add-task">
-    <div class="wrap-input">
-      <p class="label">New task</p>
-      <input type="text" placeholder="What do you want to add?" />
+    <div class="wrap-content">
+      <div class="wrap-input">
+        <p class="label">New task</p>
+        <input type="text" placeholder="What do you want to add?" />
+      </div>
+      <div class="action-btn">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path d="M12 5V19" stroke="inherit" stroke-width="2" stroke-linecap="round" />
+          <path d="M5 12L19 12" stroke="inherit" stroke-width="2" stroke-linecap="round" />
+        </svg>
+      </div>
     </div>
-    <div class="action-btn">+</div>
   </section>
   <!-- LIST COMPONENT -->
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+h1 {
+  margin: 24px 0 24px 24px;
+  font-size: 18px;
+  font-weight: 600;
+  color: var(--white);
+}
 /* LIST COMPONENT */
 .wrap-list {
-  height: 80vh;
+  height: 70vh;
   padding: 24px;
   margin: 8px;
   background-color: var(--surface-color);
@@ -33,17 +47,66 @@ import { RouterLink, RouterView } from 'vue-router'
 
   .list-item {
     display: flex;
+    gap: 16px;
+
+    p {
+      font-size: 16px;
+    }
   }
 }
 
 /* ADD TASK COMPONENT */
-.wrap-input {
-  position: sticky;
-  bottom: 8px;
-  padding: 8px;
 
-  .action-btn {
-    background-color: --surface-color;
+.wrap-add-task {
+  position: fixed;
+  bottom: 8px;
+  left: 0;
+  right: 0;
+
+  .wrap-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 24px;
+  }
+}
+.wrap-input {
+  .label {
+    margin-bottom: 8px;
+    font-size: 14px;
+  }
+  input {
+    width: 100%;
+    background-color: var(--bg-color);
+    color: var(--accent-color);
+    font-size: 18px;
+    font-weight: 400;
+    border: none;
+    outline: none;
+    &::placeholder {
+      color: var(--gray500);
+    }
+    &:focus {
+      &::placeholder {
+        color: var(--bg-color);
+      }
+    }
+  }
+}
+
+.action-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  background-color: var(--gray100);
+  border: 1px solid var(--gray150);
+  border-radius: 80px;
+  cursor: pointer;
+
+  svg {
+    stroke: var(--accent-color);
   }
 }
 </style>
