e6d308102414b403c7f54da8f90a66e84612616b
Gabs <gabs@fundz.money>
2023-12-07 11:26:23 -0300

    Adjusting css

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div :class="{ 'dark-mode': isDarkMode }">
     <h1>Task List</h1>
-    <form @submit.prevent="addTask">
-      <input v-model="newTask" type="text" placeholder="Enter a new task" />
-      <button type="submit">Add Task</button>
+    <form @submit.prevent="addTask" class="wrap-add-task">
+      <div class="wrap-content">
+        <div class="wrap-input">
+          <input v-model="newTask" type="text" placeholder="Enter a new task" />
+        </div>
+        <button type="submit" class="action-btn">Add Task</button>
+      </div>
     </form>
-    <ul>
-      <li v-for="(task, index) in tasks" :key="index">
-        <input v-model="task.title" type="text" />
-        <button @click="updateTask(task)">Update</button>
-        <button @click="deleteTask(index)">Delete</button>
-      </li>
-    </ul>
-    <button @click="toggleDarkMode">Toggle Dark Mode</button>
+    <div class="wrap-list">
+      <ul v-if="tasks.length > 0" class="list-data">
+        <li v-for="(task, index) in tasks" :key="index" class="list-item">
+          <input v-model="task.title" type="text" />
+          <button @click="updateTask(task)">Update</button>
+          <button @click="deleteTask(index)">Delete</button>
+        </li>
+      </ul>
+      <div v-else class="list-empty-state">No tasks available</div>
+    </div>
+    <!-- <button @click="toggleDarkMode">Toggle Dark Mode</button> -->
   </div>
 </template>
 
@@ -66,102 +73,150 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
-// BEGIN: be15d9bcejpp
-/* Material Design Colors */
-$primary-color: #2196f3;
-$secondary-color: #f50057;
-$background-color: #f5f5f5;
-$dark-background-color: #333333;
-
-/* Material Design Typography */
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap');
-$font-family: 'Inter', sans-serif;
-
-$font-size: 16px;
-$font-weight-light: 300;
-$font-weight-regular: 400;
-$font-weight-medium: 500;
-
-/* Material Design Button */
-$button-border-radius: 4px;
-$button-padding: 8px 16px;
-$button-primary-background-color: $primary-color;
-$button-primary-color: #ffffff;
-$button-secondary-background-color: $secondary-color;
-$button-secondary-color: #ffffff;
-
-/* Material Design Input */
-$input-border-radius: 4px;
-$input-border-color: #bdbdbd;
-$input-focus-border-color: $primary-color;
-$input-padding: 8px;
-$input-font-size: $font-size;
-
-/* Material Design List */
-$list-item-padding: 8px;
-$list-item-border-radius: 4px;
-$list-item-background-color: #ffffff;
-$list-item-dark-background-color: #222222;
-
-/* Material Design Card */
-$card-border-radius: 4px;
-$card-background-color: #ffffff;
-$card-dark-background-color: #222222;
-$card-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
-/* Apply Material Design styles */
-body {
-  background-color: $background-color;
-  font-family: $font-family;
-  font-size: $font-size;
-  font-weight: $font-weight-regular;
+h1 {
+  margin: 24px 0 24px 24px;
+  font-size: 18px;
+  font-weight: 600;
+  color: var(--white);
 }
 
-.dark-mode {
-  background-color: $dark-background-color;
-}
+/* TASK LIST */
+.wrap-list {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 8px;
+  margin: 8px;
+  background-color: var(--surface-color);
+  border-radius: 8px;
 
-button {
-  border-radius: $button-border-radius;
-  padding: $button-padding;
-  font-family: $font-family;
-  font-size: $font-size;
-  font-weight: $font-weight-medium;
-}
+  .list-data {
+    margin-top: 8px;
+  }
 
-input {
-  border-radius: $input-border-radius;
-  border: 1px solid $input-border-color;
-  padding: $input-padding;
-  font-size: $input-font-size;
+  .list-empty-state {
+    height: 30%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--gray500);
+  }
 }
+/* END TASK LIST */
 
-ul {
-  list-style-type: none;
-  padding: 0;
+/* ADD TASK INPUT */
+.wrap-add-task {
+  .wrap-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 8px;
+  }
 }
+.wrap-input {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
-li {
-  padding: $list-item-padding;
-  border-radius: $list-item-border-radius;
-  background-color: $list-item-background-color;
-  box-shadow: $card-box-shadow;
+  svg {
+    width: 24px;
+    height: 24px;
+    stroke: var(--accent-color);
+  }
+
+  input {
+    background-color: var(--surface-color);
+    padding: 12px 16px;
+    color: var(--white);
+    font-size: 18px;
+    font-weight: 400;
+    border: none;
+    border-radius: 8px;
+    outline: none;
+    &::placeholder {
+      color: var(--gray600);
+    }
+    &:focus {
+      &::placeholder {
+        color: transparent;
+      }
+    }
+  }
 }
+.action-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  background-color: var(--gray100);
+  border: 1px solid var(--gray150);
+  border-radius: 80px;
+  cursor: pointer;
 
-.card {
-  border-radius: $card-border-radius;
-  background-color: $card-background-color;
-  box-shadow: $card-box-shadow;
+  svg {
+    stroke: var(--accent-color);
+  }
 }
+/* END ADD TASK INPUT */
 
-.dark-mode button,
-.dark-mode input,
-.dark-mode li {
-  background-color: $list-item-dark-background-color;
-  color: #ffffff;
-}
+.list-item {
+  display: flex;
+  gap: 16px;
+  margin-bottom: 8px;
+  background-color: var(--gray200);
+  padding: 16px;
+  border-radius: 6px;
 
-.dark-mode .card {
-  background-color: $card-dark-background-color;
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  input[type='checkbox'] {
+    appearance: none;
+    font: inherit;
+    color: currentColor;
+    min-width: 16px;
+    width: 16px;
+    height: 16px;
+    border: 0.15em solid var(--gray500);
+    border-radius: 0.15em;
+    cursor: pointer;
+    margin-top: 2px;
+
+    display: grid;
+    place-content: center;
+  }
+
+  input[type='checkbox']::before {
+    content: '';
+    width: 0.5em;
+    height: 0.5em;
+    transform: scale(0);
+    transition: 100ms transform ease-in-out;
+    box-shadow: inset 1em 1em var(--white);
+  }
+
+  input[type='checkbox']:checked::before {
+    transform: scale(1);
+  }
+
+  .content {
+    flex: 1;
+    font-size: 16px;
+  }
+
+  svg {
+    stroke: var(--gray600);
+    width: 16px;
+    height: 16px;
+    margin-top: 2px;
+    cursor: pointer;
+
+    &:hover {
+      stroke: var(--white);
+    }
+  }
 }
 </style>
