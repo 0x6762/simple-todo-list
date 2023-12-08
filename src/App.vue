318ec82f2d2068a956a492d0dd010c04a3941ac1
Skakashi <1717634+skakashi@users.noreply.github.com>
2023-12-08 10:55:35 -0300

    DRAFT - Copilot test (#4)
    * WIP
    
    * Rename variables
    
    * clean up TaskItem component
    
    * align icons
    
    * Add arrow icon
    
    * WIP
    
    * WIP
    
    * Adjusting css
    
    * Add icons
    
    * Fixed edit interaction
    
    * Completed tasks now move to a new array
    
    * Completed tasks now are simple text, fix ordering after removing task
    
    * Remove empty state
    
    * Finishing touches
    
    ---------
    
    Co-authored-by: Gabs <gabs@fundz.money>

--- src/App.vue
+++ src/App.vue
@@ -1,140 +1,189 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import TaskItem from './components/TaskItem.vue'
-
-defineProps(['title'])
-
-const STORAGE_KEY = 'todo-app-storage'
-onMounted(() => {
-  tasks.value = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
-})
-
-// Sets type to item
-type item = {
-  id: number
-  title: string
-}
-
-// Sets an empty array to receive data
-const tasks = ref<item[]>([])
-
-// Sets an empty string to receive input value
-const newItem = ref('')
-
-// Add task to list
-const addTask = () => {
-  tasks.value.push({ id: tasks.value.length + 1, title: newItem.value })
-  newItem.value = ''
-  // Add task to local storage
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks.value))
-}
-// Remove task from list
-const removeTask = (item) => {
-  tasks.value.splice(tasks.value.indexOf(item), 1)
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks.value))
-}
-</script>
-
 <template>
-  <div class="main">
-    <header>
-      <h1 class="list-name">My tasks</h1>
-    </header>
-
-    <!-- TASK LIST -->
-    <section class="wrap-list">
-      <ul class="list-data">
-        <TaskItem
-          v-for="item in tasks"
-          :key="item.id"
-          :title="item.title"
-          @remove-task="removeTask(item)"
-        >
-        </TaskItem>
-      </ul>
-      <div class="list-empty-state" v-if="!tasks.length">
-        <p>Nothing here yet</p>
-      </div>
-      <!-- <button @click="removeTask">remove</button> -->
-    </section>
-    <!-- END TASK LIST -->
-
-    <!-- ADD TASK INPUT -->
-    <section class="wrap-add-task">
-      <form class="wrap-content" @submit.prevent="addTask">
-        <div class="wrap-input">
-          <p class="label">New task</p>
-          <input v-model.trim="newItem" type="text" placeholder="What do you want to add?" />
+  <!-- <div :class="{ 'dark-mode': isDarkMode }"> -->
+  <div>
+    <h1>Task List</h1>
+    <div class="wrap-list">
+      <form @submit.prevent="addTask" class="wrap-add-task">
+        <div class="wrap-content">
+          <div class="wrap-input">
+            <ArrowIcon />
+            <input v-model="newTask" type="text" placeholder="Enter a new task" />
+          </div>
         </div>
-        <button class="action-btn">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M12 5V19" stroke="inherit" stroke-width="2" stroke-linecap="round" />
-            <path d="M5 12L19 12" stroke="inherit" stroke-width="2" stroke-linecap="round" />
-          </svg>
-        </button>
       </form>
-    </section>
-    <!-- END ADD TASK INPUT -->
+      <ul v-if="activeTasks.length > 0 || completedTasks.length > 0" class="list-data">
+        <div class="list-effect">
+          <transition-group name="list">
+            <li v-for="(task, index) in activeTasks" :key="task.id" class="list-item">
+              <input
+                type="checkbox"
+                class="checkbox"
+                :checked="task.completed"
+                @change="completeTask(task)"
+              />
+              <input
+                :class="{ content: true, completed: task.completed }"
+                v-model="task.title"
+                type="text"
+                @keyup.enter="updateTask(task), $event.target.blur()"
+                @blur="updateTask(task)"
+              />
+              <CloseIcon @click="deleteTask(index)" />
+            </li>
+          </transition-group>
+        </div>
+        <!-- Completed tasks -->
+        <p class="completed-task-title" v-if="completedTasks.length > 0">Completed tasks</p>
+        <li v-for="(task, index) in completedTasks" :key="task.id" class="list-item">
+          <input
+            type="checkbox"
+            class="checkbox"
+            :checked="task.completed"
+            @change="completeTask(task)"
+          />
+          <p :class="{ content: true, completed: task.completed }">{{ task.title }}</p>
+        </li>
+        <!-- End Completed tasks -->
+      </ul>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-h1 {
-  margin: 24px 0 24px 24px;
-  font-size: 18px;
-  font-weight: 600;
-  color: var(--white);
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import ArrowIcon from './components/icons/ic_arrow.svg'
+import CloseIcon from './components/icons/ic_close.svg'
+
+interface Task {
+  title: string
+  completed: boolean
 }
 
-/* TASK LIST */
+const activeTasks = ref<Task[]>([])
+const completedTasks = ref<Task[]>([])
+const newTask = ref('')
+
+// const isDarkMode = ref(false)
+
+function addTask() {
+  const task = {
+    id: Date.now(), // Use the current timestamp as a unique id
+    title: newTask.value,
+    completed: false
+  }
+  activeTasks.value.push(task)
+  newTask.value = ''
+  orderTasksById()
+  saveTasksToLocalStorage()
+}
+
+function updateTask(task: Task) {
+  task.title = task.title.trim()
+  saveTasksToLocalStorage()
+}
+
+function deleteTask(index: number) {
+  activeTasks.value.splice(index, 1)
+  saveTasksToLocalStorage()
+}
+
+function completeTask(task: Task) {
+  task.completed = !task.completed
+  if (task.completed) {
+    const index = activeTasks.value.indexOf(task)
+    activeTasks.value.splice(index, 1)
+    completedTasks.value.push(task)
+  } else {
+    const index = completedTasks.value.indexOf(task)
+    completedTasks.value.splice(index, 1)
+    activeTasks.value.unshift(task)
+    orderTasksById()
+  }
+  saveTasksToLocalStorage()
+}
+
+function orderTasksById() {
+  activeTasks.value.sort((a, b) => a.id - b.id)
+}
+
+function saveTasksToLocalStorage() {
+  localStorage.setItem('activeTasks', JSON.stringify(activeTasks.value))
+  localStorage.setItem('completedTasks', JSON.stringify(completedTasks.value))
+}
+
+function loadTasksFromLocalStorage() {
+  const savedActiveTasks = localStorage.getItem('activeTasks')
+  const savedCompletedTasks = localStorage.getItem('completedTasks')
+  if (savedActiveTasks) {
+    activeTasks.value = JSON.parse(savedActiveTasks)
+  }
+  if (savedCompletedTasks) {
+    completedTasks.value = JSON.parse(savedCompletedTasks)
+  }
+}
+
+// function toggleDarkMode() {
+//   isDarkMode.value = !isDarkMode.value
+// }
+
+onMounted(() => {
+  loadTasksFromLocalStorage()
+})
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 18px;
+  font-weight: 400;
+  margin: 24px 0 24px 24px;
+}
+
 .wrap-list {
-  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  position: relative;
   padding: 8px;
   margin: 8px;
   background-color: var(--surface-color);
   border-radius: 8px;
-
-  .list-empty-state {
-    height: 30%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: var(--gray500);
-  }
 }
-/* END TASK LIST */
 
 /* ADD TASK INPUT */
 .wrap-add-task {
-  width: 100%;
-  position: fixed;
-  bottom: 8px;
-
   .wrap-content {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 24px;
+    padding: 0 8px;
   }
 }
 .wrap-input {
-  .label {
-    margin-bottom: 8px;
-    font-size: 14px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  svg {
+    width: 24px;
+    height: 24px;
+    stroke: var(--accent-color);
   }
+
   input {
-    background-color: var(--bg-color);
-    color: var(--accent-color);
+    background-color: var(--surface-color);
+    padding: 12px 16px;
+    color: var(--white);
     font-size: 18px;
     font-weight: 400;
     border: none;
+    border-radius: 8px;
     outline: none;
     &::placeholder {
-      color: var(--gray500);
+      color: var(--gray600);
     }
     &:focus {
       &::placeholder {
-        color: var(--bg-color);
+        color: transparent;
       }
     }
   }
@@ -155,4 +204,107 @@ h1 {
   }
 }
 /* END ADD TASK INPUT */
+
+.list-data {
+  margin-top: 8px;
+}
+
+.list-empty-state {
+  height: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--gray500);
+}
+
+.completed-task-title {
+  font-size: 14px;
+  margin-top: 24px;
+  margin-bottom: 16px;
+  color: var(--gray900);
+}
+
+.list-effect {
+  .list-enter-active,
+  .list-leave-active {
+    transition: all 0.3s ease;
+  }
+  .list-enter-from,
+  .list-leave-to {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+}
+
+.list-item {
+  display: flex;
+  gap: 16px;
+  margin-bottom: 8px;
+  background-color: var(--gray200);
+  padding: 16px;
+  border-radius: 6px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  .checkbox {
+    margin-right: 8px;
+  }
+
+  input[type='checkbox'] {
+    appearance: none;
+    font: inherit;
+    color: currentColor;
+    min-width: 16px;
+    width: 16px;
+    height: 16px;
+    border: 0.15em solid var(--gray500);
+    border-radius: 0.15em;
+    cursor: pointer;
+    margin-top: 2px;
+
+    display: grid;
+    place-content: center;
+  }
+
+  input[type='checkbox']::before {
+    content: '';
+    width: 0.5em;
+    height: 0.5em;
+    transform: scale(0);
+    transition: 100ms transform ease-in-out;
+    box-shadow: inset 1em 1em var(--white);
+  }
+
+  input[type='checkbox']:checked::before {
+    transform: scale(1);
+  }
+
+  .content {
+    flex: 1;
+    font-size: 16px;
+    background-color: transparent;
+    color: var(--white);
+    border: none;
+    outline: none;
+  }
+
+  .completed {
+    text-decoration: line-through;
+    color: var(--gray500);
+  }
+
+  svg {
+    stroke: var(--gray600);
+    width: 16px;
+    height: 16px;
+    margin-top: 2px;
+    cursor: pointer;
+
+    &:hover {
+      stroke: var(--white);
+    }
+  }
+}
 </style>
--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,90 +1,115 @@
-<script setup lang="ts">
-defineProps(['title'])
-defineEmits(['removeTask'])
-</script>
-
 <template>
   <li class="list-item">
-    <div class="wrap-content">
-      <input type="checkbox" />
-      <p class="content">{{ title }}</p>
-    </div>
-    <button @click="$emit('removeTask')" class="remove-item">
-      <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
-        <path
-          d="M18 6L6 18"
-          stroke="inherit"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M6 6L18 18"
-          stroke="inherit"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </button>
+    <input type="checkbox" />
+    <template v-if="!isEditing.value">
+      <p class="content">{{ task.title }}</p>
+      <CloseIcon @click="$emit('removeTask')" class="remove-item" />
+      <EditIcon @click="startEditingTask" class="edit-item" />
+    </template>
+    <template v-else>
+      <input
+        v-model="editedTitle"
+        class="edit-input"
+        @keyup.enter="updateTask"
+        @blur="cancelEditingTask"
+      />
+      <CloseIcon @click="cancelEditingTask" class="cancel-item" />
+    </template>
   </li>
 </template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue' // Import reactive here
+
+import CloseIcon from './icons/ic_close.svg'
+import EditIcon from './icons/ic_arrow.svg'
+
+const props = defineProps({
+  task: Object,
+  isTaskEditing: Boolean
+})
+
+const emit = defineEmits(['removeTask', 'startEditingTask', 'updateTask', 'cancelEditingTask'])
+
+const { editedTitle } = reactive({
+  editedTitle: props.task.title
+})
+
+const isEditing = ref(false) // Separate ref for isEditing state
+
+const startEditingTask = () => {
+  emit('startEditingTask', props.task)
+  isEditing.value = true
+}
+
+const cancelEditingTask = () => {
+  editedTitle.value = props.task.title
+  emit('cancelEditingTask')
+  isEditing.value = false
+}
+
+const updateTask = () => {
+  emit('updateTask', { ...props.task, title: editedTitle.value })
+  isEditing.value = false
+}
+</script>
 
 <style lang="scss">
 .list-item {
   display: flex;
   gap: 16px;
-  justify-content: space-between;
-  align-items: baseline;
   margin-bottom: 8px;
   background-color: var(--gray200);
   padding: 16px;
   border-radius: 6px;
 
-  .wrap-content {
-    display: flex;
-    align-items: baseline;
-    gap: 16px;
-
-    input[type='checkbox'] {
-      appearance: none;
-      font: inherit;
-      color: currentColor;
-      min-width: 16px;
-      width: 16px;
-      height: 16px;
-      border: 0.15em solid currentColor;
-      border-radius: 0.15em;
-      cursor: pointer;
-
-      display: grid;
-      place-content: center;
-    }
-
-    input[type='checkbox']::before {
-      content: '';
-      width: 0.5em;
-      height: 0.5em;
-      transform: scale(0);
-      transition: 100ms transform ease-in-out;
-      box-shadow: inset 1em 1em var(--white);
-    }
-
-    input[type='checkbox']:checked::before {
-      transform: scale(1);
-    }
-
-    p {
-      font-size: 16px;
-    }
+  &:last-child {
+    margin-bottom: 0;
   }
 
-  button {
-    background-color: transparent;
+  input[type='checkbox'] {
+    appearance: none;
+    font: inherit;
+    color: currentColor;
+    min-width: 16px;
+    width: 16px;
+    height: 16px;
+    border: 0.15em solid var(--gray500);
+    border-radius: 0.15em;
+    cursor: pointer;
+    margin-top: 2px;
+
+    display: grid;
+    place-content: center;
+  }
+
+  input[type='checkbox']::before {
+    content: '';
+    width: 0.5em;
+    height: 0.5em;
+    transform: scale(0);
+    transition: 100ms transform ease-in-out;
+    box-shadow: inset 1em 1em var(--white);
+  }
+
+  input[type='checkbox']:checked::before {
+    transform: scale(1);
+  }
+
+  .content {
+    flex: 1;
+    font-size: 16px;
+  }
+
+  svg {
+    stroke: var(--gray600);
+    width: 16px;
+    height: 16px;
+    margin-top: 2px;
     cursor: pointer;
 
-    svg {
-      stroke: var(--gray500);
+    &:hover {
+      stroke: var(--white);
     }
   }
 }
